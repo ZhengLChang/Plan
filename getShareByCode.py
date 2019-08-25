2aba1bbec57783b3460d5020182ff914ecaea739
zhenghuijie <13007568302@163.com>
2019-08-25 17:10:35 +0800

    update getShareByCode

--- getShareByCode.py
+++ getShareByCode.py
@@ -7,22 +7,24 @@ headers = {
 	'User-Agent': 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)',
 	'Host': 'api-wows-wscn.xuangubao.cn'
 }
-argvLen = len(sys.argv)
-for codeName in sys.argv[1:]:
-##############根据传入名称，得到对应代码#######################
-	searchReq = request.Request(url='http://api-wows-wscn.xuangubao.cn' + '/v3/search/stocks?q=' + codeName, headers=headers, method='GET')
+def getShareCodeByName(shareName):
+	searchReq = request.Request(url='http://api-wows-wscn.xuangubao.cn' + '/v3/search/stocks?q=' + shareName, headers=headers, method='GET')
 	searchResponse = request.urlopen(searchReq)
 	searchJson = json.loads(searchResponse.read().decode('utf-8'))
-#	print(type(searchJson['data']['stocks']))
-################遍历结果集##############################
-	for searchJsonCode in searchJson['data']['stocks']:
-		curShareReq = request.Request(url='http://api-ddc-wscn.xuangubao.cn/market/real?fields=symbol,prod_name,prod_en_name,price_precision,px_change_rate,px_change,last_px&prod_code=' + searchJsonCode['code'], headers=headers, method='GET')
-		curShareResponse = request.urlopen(curShareReq)
-		curShareJson = json.loads(curShareResponse.read().decode('utf-8'))
-		
-		curShareSet = curShareJson['data']['snapshot']
-################处理返回的结果##############################
-		for curShareDot in curShareSet:
-			print("%s %s" % (round(curShareSet[curShareDot][4], 2),curShareSet[curShareDot][6]))
+	return searchJson['data']['stocks']
 
+def getShareStatusByCode(shareCode):
+	curShareReq = request.Request(url='http://api-ddc-wscn.xuangubao.cn/market/real?fields=symbol,prod_name,prod_en_name,price_precision,px_change_rate,px_change,last_px&prod_code=' + shareCode, headers=headers, method='GET')
+	curShareResponse = request.urlopen(curShareReq)
+	curShareJson = json.loads(curShareResponse.read().decode('utf-8'))
+	curShareSet = curShareJson['data']['snapshot']
+################处理返回的结果#############################
+	for curShareDot in curShareSet:
+		print("%s %s" % (round(curShareSet[curShareDot][4], 2),curShareSet[curShareDot][6]))
+
+
+argvLen = len(sys.argv)
+for shareName in sys.argv[1:]:
+	for searchJsonCode in getShareCodeByName(shareName):
+		getShareStatusByCode(searchJsonCode['code'])
 
