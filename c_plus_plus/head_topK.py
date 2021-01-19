3e9971e82a5cbd908343ef842a5dcbc99d277172
zheng <zhengtianjie@me.com>
2021-01-19 01:39:34 +0000

    debug

--- c_plus_plus/head_topK.py
+++ c_plus_plus/head_topK.py
@@ -1,14 +1,14 @@
 from io import StringIO
 import math
 
-class head_topK(object):
-    K = 30
-    def __init__(self, init_arr=list()):
+class heap_topK(object):
+    def __init__(self, init_arr=list(), K = 10):
         self.arr = list(set(init_arr))
+        self.K = K
         for num in self.arr:
             self.add(num)
 
-    def head_adjust(self, arr, arr_len, index):
+    def heap_adjust(self, arr, arr_len, index):
         leftIdx = 2 * index + 1
         rightIdx = 2 * index + 2
 
@@ -24,13 +24,22 @@ class head_topK(object):
             tmp = arr[index]
             arr[index] = arr[maxIdx]
             arr[maxIdx] = tmp
-            self.head_adjust(arr, arr_len, maxIdx)
+            self.heap_adjust(arr, arr_len, maxIdx)
 
-    def min_head(self):
+    def result(self):
+        tmp_arr = list(self.arr)
+        for i in range(len(tmp_arr) - 1, -1, -1):
+            tmp = tmp_arr[0]
+            tmp_arr[0] = tmp_arr[i]
+            tmp_arr[i] = tmp
+            self.heap_adjust(tmp_arr, i, 0)
+        return tmp_arr
+
+    def heap_sort(self):
         arr_len = len(self.arr)
         last_non_leaf_node = int(arr_len / 2) - 1
         for i in range(last_non_leaf_node, -1, -1):
-            self.head_adjust(self.arr, arr_len, i)
+            self.heap_adjust(self.arr, arr_len, i)
         return self.arr
 
     def add(self, new_num):
@@ -41,13 +50,13 @@ class head_topK(object):
                 self.arr.append(new_num) 
                 return 
 
-            if len(self.arr) < head_topK.K:
+            if len(self.arr) < self.K:
                 self.arr.append(new_num)
-                self.min_head()
+                self.heap_sort()
                 return
             if new_num > self.arr[0]:
                 self.arr[0] = new_num
-                self.head_adjust(self.arr, len(self.arr), 0)
+                self.heap_adjust(self.arr, len(self.arr), 0)
                 return
         elif type(new_num) == list:
             for num in new_num:
@@ -75,12 +84,13 @@ class head_topK(object):
 
 
 if __name__ == "__main__":
-    obj = head_topK([8, 5])
-    a = [22, 1, 19, 9, 4, 10, 11]
-    obj.add(a)
-    #for i in a:
-    #    obj.add(i)
+    obj = heap_topK([8, 5])
+    a = [2, 22, 1, 19, 9, 4, 10, 11]
+    #obj.add(a)
+    for i in a:
+        obj.add(i)
     obj.show_tree()
+    print(obj.result())
 
 
 
