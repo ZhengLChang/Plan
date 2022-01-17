aa5fcefc793a78a30a76f5fad1f242deb1a86e33
zheng <zhengtianjie@me.com>
2022-01-17 13:46:07 +0800

    更新接口

--- getShareByCode.py
+++ getShareByCode.py
@@ -2,24 +2,25 @@
 import sys
 from urllib import request
 import json
+import requests
 
 class Share(object):
   def __init__(self):
     self.headers  = {
 	  'User-Agent': 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)',
-	  'Host': 'api-wows-wscn.xuangubao.cn'
+	  #'Host': 'api-wows-wscn.xuangubao.cn'
+          'Host': 'api-ddc-wscn.xuangubao.cn',
     }
   def getShareCodeByName(self, shareName):
-    searchReq = request.Request(url='http://api-wows-wscn.xuangubao.cn' + '/v3/search/stocks?q=' + shareName, headers=self.headers, method='GET')
-    searchResponse = request.urlopen(searchReq)
-    searchJson = json.loads(searchResponse.read().decode('utf-8'))
+    searchResponse = requests.get(url='http://api-wows-wscn.xuangubao.cn' + '/v3/search/stocks?q=' + shareName, headers=self.headers)
+    searchJson = json.loads(searchResponse.text)
     return searchJson['data']['stocks']
 
   def getShareStatusByCode(self, shareCode):
-    #print(shareCode)
-    curShareReq = request.Request(url='http://api-ddc-wscn.xuangubao.cn/market/real?fields=symbol,prod_name,prod_en_name,price_precision,px_change_rate,px_change,last_px&prod_code=' + shareCode, headers=self.headers, method='GET')
-    curShareResponse = request.urlopen(curShareReq)
-    curShareJson = json.loads(curShareResponse.read().decode('utf-8'))
+    curShareResponse = requests.get(url='https://api-ddc-wscn.xuangubao.cn/market/real?fields=symbol,prod_name,prod_en_name,price_precision,px_change_rate,px_change,last_px&prod_code=' + shareCode, headers=self.headers);
+
+    #print(curShareResponse.text)
+    curShareJson = json.loads(curShareResponse.text)
     curShareSet = curShareJson['data']['snapshot']
 ################处理返回的结果#############################
     for curShareDot in curShareSet:
